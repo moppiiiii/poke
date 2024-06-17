fb7eca8f7f19d008f53e895c872666884bec63c9
moppiiiii <tnktmhssrc@gmail.com>
2024-06-17 20:53:00 +0900

    feat: sb-liquidation

---
+++ apps/frontend/src/app/components/templates/poke-book/_data/pokemon-detail.data.ts
@@ -0,0 +1,10 @@
+export const POKEMON_DETAIL = {
+  id: 1,
+  name: 'bulbasaur',
+  height: 7,
+  weight: 69,
+  types: ['grass', 'poison'],
+  criesSource: 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/1.ogg',
+  frontImage: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+  backImage: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png',
+};
